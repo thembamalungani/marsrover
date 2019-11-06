a5afd4f3f3a1caef190998d084e7a48f9547a457
Themba Malungani <themba.clarence@gmail.com>
2019-11-06 18:16:38 +0200

    add comments

--- src/main/java/com/tmalungani/marsrover/ICommandParser.java
+++ src/main/java/com/tmalungani/marsrover/ICommandParser.java
@@ -1,8 +1,31 @@
 package com.tmalungani.marsrover;
 
 public interface ICommandParser {
-
-    Position position(String startingPosition) throws IllegalRoverPositionException, IllegalRoverDirectionException;
+    /**
+     * Parses the size of the grid from string to Grid.Size
+     *
+     * @param String size
+     * @return Grid.Size
+     * @throws IllegalGridSizeException
+     */
     Grid.Size size(String size) throws IllegalGridSizeException;
+
+    /**
+     * Parses the commands to be sent to rover
+     *
+     * @param String commands
+     * @return String
+     * @throws IllegalRoverCommandException
+     */
     String commands(String commands) throws IllegalRoverCommandException;
+
+    /**
+     * Parses the position of rover from string to Position
+     *
+     * @param String startingPosition
+     * @return
+     * @throws IllegalRoverPositionException
+     * @throws IllegalRoverDirectionException
+     */
+    Position position(String startingPosition) throws IllegalRoverPositionException, IllegalRoverDirectionException;
 }
